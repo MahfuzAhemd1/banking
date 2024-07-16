b998c39452344b22764e0989cd296faabb4aec07
Mahfuz Ahemd Nirob <mahfuzahmednirob1@gmail.com>
2024-07-17 03:17:41 +0600

    implemented total balance animation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+  import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
+  const loggedIn = { firstName:'Sornir'}
   return (
-    <div>Home</div>
+    <section className="home">
+       <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and manage your account and transactions easily"
+
+          />
+
+          <TotalBalanceBox 
+          
+          accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={125.25}
+          />
+        </header>
+
+
+       </div>
+
+    </section>
   )
 }
 
