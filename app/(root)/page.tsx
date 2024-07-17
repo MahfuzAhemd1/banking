b4f445d9faf7e2ceed408825690699e00eb66bd5
Mahfuz Ahemd Nirob <mahfuzahmednirob1@gmail.com>
2024-07-17 23:51:24 +0600

    implemented right side bar and added bank card links - only pictures

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
   import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
-  const loggedIn = { firstName:'Sornir',}
+  const loggedIn = { firstName:'Sornir', lastName: 'Ahmed', email:'sornir@gmail.com'}
   return (
     <section className="home">
        <div className="home-content">
@@ -24,9 +25,14 @@ const Home = () => {
           />
         </header>
 
-
+      RECENT TRANSACTION
        </div>
 
+       <RightSidebar  user={loggedIn}
+       transactions={[]}
+       banks={[{currentBalance:123.50},{currentBalance:500.50}]}
+       />
+      
     </section>
   )
 }
